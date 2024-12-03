5449fcf36bc195f36dc25879db2cc273472b6183
Kenny.G <151454821+Kenny-204@users.noreply.github.com>
2024-12-03 23:40:25 +0100

    Update server_class.ts

--- src/server_class.ts
+++ src/server_class.ts
@@ -21,7 +21,7 @@ export class Server {
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(
       cors({
-        origin: "http://localhost:5173",
+        origin: "https://streampot-frontend.onrender.com",
         methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: true,
       })
